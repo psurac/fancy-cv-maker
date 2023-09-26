d3561ba9db3b78e5cd10610dc5e45c2a8b350915
psurac <philipsurac@web.de>
2023-09-26 22:51:52 +0300

    Intial commit for fancy-cv-maker

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [ data, setData ] = useState();
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {!data ? "Loading..." : data}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PORT = process.env.PORT || 3001;
+const app = (0, express_1.default)();
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from the server" });
+});
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
