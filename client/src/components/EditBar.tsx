3d0f324b355193059dfab3b5c2f7335a0ed361a2
psurac <philipsurac@web.de>
2023-10-05 17:34:52 +0300

    Added some code for EditBar to solve a error. New Issue occured

--- client/src/components/EditBar.tsx
+++ client/src/components/EditBar.tsx
@@ -1,12 +1,16 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import './EditBar.scss';
 import useFetch from '../hooks/useFetch';
 
 const EditBar: FC = () => {
+    const formElement = useRef<HTMLFormElement | null >(null);
     const [fetchFunction, data] = useFetch();
-    const changeHanler = (e: React.ChangeEvent<HTMLFormElement>) => {
+
+    const changeHandler = () => formElement.current?.submit();
+
+    const submitHandler = (e: React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const formData = new FormData(e.target)
+        const formData = new FormData(e.target);
         console.log(formData);
         if (fetchFunction) {
             fetchFunction('POST', '/cv-style', formData);
@@ -15,7 +19,13 @@ const EditBar: FC = () => {
     }
     return (
         <div className="edit-bar-container">
-            <form className='select-style' onChange={changeHanler}>
+            <form
+                ref={formElement}
+                className='select-style'
+                onSubmit={submitHandler}
+                onChange={changeHandler}
+                method='post'
+            >
                 <select name='cv-style' id='cv-style'>
                     <option value='windows'>Windows</option>
                     <option value='mac'>Mac</option>
