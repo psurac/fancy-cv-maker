49a02b9b74b4cea356d1d69d7dfcab01da5f5c99
psurac <philipsurac@web.de>
2023-10-06 17:12:27 +0300

    Added the boxes for the cv-page

--- client/src/components/CVPage.tsx
+++ client/src/components/CVPage.tsx
@@ -2,13 +2,16 @@ import { FC } from 'react';
 import './CVPage.scss';
 
 const CVPage: FC = () => {
-    const gridBoxes: number = 3 * 12;
+    const gridBoxes: JSX.Element[] = Array(2*12).fill(0).map( (_, index) => (
+        <div className={`box-${index+1}`}>{index+1}</div>
+    ));
+
     return (
         <div className="page">
             <div className="navbar-element">
                 Navbar Element
             </div>
-            {}
+            {[gridBoxes]}
             <div className="taskbar-element">
                 Taskbar Element
             </div>
