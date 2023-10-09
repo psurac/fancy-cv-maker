5f969e5c7b2ecec8c0c00ca50a9c1b70647c3a17
psurac <philipsurac@web.de>
2023-10-10 00:08:31 +0300

    Added the hooks for drag and drop in the components.

--- client/src/elements/Box.tsx
+++ client/src/elements/Box.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
+import { useDrop } from 'react-dnd';
+import { ItemTypes } from '../constants/ItemTypes';
 
-const Box: FC<any> = ( {prop} ) => {
+const Box: FC<any> = ( {prop, children} ) => {
+    const [, drop] = useDrop<any>(() => ({
+        accept: ItemTypes.BOX,
+        collect: monitor => ({
+            isOver: !!monitor.isOver(),
+          }),
+        })
+    );
     return (
-        <>
+        <div ref={drop}>
             {prop}
-        </>
+        </div>
     );
 };
 
--- client/src/elements/ListUO.tsx
+++ client/src/elements/ListUO.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react';
+import { useDrag, DragSourceMonitor } from 'react-dnd';
+import { ItemTypes } from '../constants/ItemTypes';
 
 const ListUO: FC = () => {
+    const [{isDragging}, drag] = useDrag(() => ({
+        type: ItemTypes.BOX,
+        collect: (monitor: DragSourceMonitor) => ({
+            isDragging: !!monitor.isDragging(),
+        })
+    }))
     return (
         <>
-            <ul>
+            <ul ref={drag}>
                 <li>Lorem</li>
                 <li>Lorem2</li>
             </ul>
