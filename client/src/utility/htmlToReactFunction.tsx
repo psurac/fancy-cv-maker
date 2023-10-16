cc910a0a958bc14f97231d480b05b30030c26da0
psurac <philipsurac@web.de>
2023-10-16 22:26:25 +0300

    Started newly in htmlToReactFunction. To use reusable functions and to use a recursion

--- client/src/utility/htmlToReactFunction.tsx
+++ client/src/utility/htmlToReactFunction.tsx
@@ -1,56 +1,42 @@
 import { FC, createElement } from 'react'
 
+
+const htmlNameTag: (htmlFrag: string) => string = (htmlFrag) => {
+    let tagName: string = '';
+    if (htmlFrag === '<>') return 'div';
+    const htmlSplitted = htmlFrag.split(' ')
+    if (htmlSplitted.length > 1) tagName = htmlSplitted[0].slice(1);
+    else tagName = htmlSplitted[0].slice(1, htmlSplitted[0].length - 1);
+    return tagName;
+}
+
+const propsToObject: (props: string) => Object = (props) => {
+    return {};
+};
+
+const childCreator: (htmlFrag: string) => Array<any> = (htmlFrag) => {
+    return [];
+};
+
+const NewComp: (html: string) => JSX.Element = (html: string) => {
+    html = html.trim();
+    const mainHtml = html.slice(0, html.indexOf('>') + 1);
+    let mainHtmlTag: string = htmlNameTag(mainHtml);
+    let mainHtmlTagProps: object = propsToObject(mainHtml);
+    let childArray: any[] = childCreator(html.slice(html.indexOf('>') + 1));
+
+    return (
+    <>
+        {createElement(mainHtmlTag, mainHtmlTagProps, childArray)}
+    </>
+)}
+
 /*
 This Function translate a html string into a React component Function,
 and return it.
 */
-
 const htmlToReactFunction = (html: string) => {
-    const NewComp: FC = () => {
-        const lengthHtml = html.length;
-        let mainHtmlTag: string = 'div';
-        let mainHtmlTagProps: object = {};
-        let renderArray: any[] = [];
-
-        let inHtmlTag: boolean = false;
-        let inCloseHtmlTag: boolean = false;
-        let htmlTag: boolean = false;
-        let inProp: boolean = false;
-        let propsObject: Object = {};
-        let actualReadedTag: string = '';
-        let actualReadedProp: string = '';
-        let actualReadedValue: string = '';
-
-        let indexHelper: number = 0;
-
-        for (let i = 0; i < lengthHtml; i++) {
-            const actualChar: string = html[i];
-            const nextChar: string = html[i + 1] ? html[i + 1] : '';
-            if (i > 0 && html[i - 1] !== '\\') {
-                if (actualChar === '<') {
-                    if (nextChar === '/') {
-                        inCloseHtmlTag = true;
-                    } else {
-                        inHtmlTag = true;
-                        htmlTag = true;
-                    }
-                }
-                if (actualChar === '>') {
-                    inCloseHtmlTag = false;
-                    inHtmlTag = false;
-                    indexHelper = i < lengthHtml ? i + 1 : 0;
-                }
-            }
-            
-
-        }
-
-        return (
-        <>
-            {createElement(mainHtmlTag, mainHtmlTagProps, renderArray)}
-        </>
-    )}
-    return NewComp;
+    return NewComp(html);
 };
 
 export default htmlToReactFunction;
