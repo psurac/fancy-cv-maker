f451dd15a3ee14e0cbefd6306159095384987b4b
psurac <philipsurac@web.de>
2023-10-18 23:21:45 +0300

    Finished the htmlTagSearchHelper so far in htmlToReactFunction

--- client/src/utility/htmlToReactFunction.tsx
+++ client/src/utility/htmlToReactFunction.tsx
@@ -6,17 +6,23 @@ type propsObj = {
 
 const htmlNameTag: (htmlFrag: string) => string = (htmlFrag) => {
     let tagName: string = '';
-    if (htmlFrag === '<>') return 'div';
+    if (htmlFrag === '<>') {
+        return 'div';
+    }
     const htmlSplitted = htmlFrag.split(' ')
-    if (htmlSplitted.length > 1) tagName = htmlSplitted[0].slice(1);
-    else tagName = htmlSplitted[0].slice(1, htmlSplitted[0].length - 1);
+    if (htmlSplitted.length > 1) {
+        tagName = htmlSplitted[0].slice(1);
+    }
+    else {
+        tagName = htmlSplitted[0].slice(1, htmlSplitted[0].length - 1);
+    }
     return tagName;
 }
 
 const propsToObject: (props: string) => Object = (props) => {
     let propsObj: propsObj = {};
     const propsNames: RegExpMatchArray | null = props.match(/(?<=\s)[-a-zA-Z0-9_]+(?=\=)/);
-    const propsValues: RegExpMatchArray | null = props.match(/(?<=[\'\"\`\{])[\s-a-zA-Z0-9_]*(?=[\'\"\`\}])/)
+    const propsValues: RegExpMatchArray | null = props.match(/(?<=[\'\"\`\{])[\s-a-zA-Z0-9_]*(?=[\'\"\`\}])/);
     if (propsNames && propsValues && propsNames.length === propsValues.length) {
         for (let i = 0; i < propsNames.length; i++) {
             propsObj[propsNames[i]] = propsValues[i];
@@ -26,6 +32,7 @@ const propsToObject: (props: string) => Object = (props) => {
 };
 
 const htmlTagSearchHelper: (htmlFrag: string) => number[] = (htmlFrag) => {
+    let siblingIndexes: number[] = [];
     // Tack the starting tagName, set counterTags to 1, absolutIndex to 0
     let absolutIndex: number = 0; // keap track on the absolut position wher htmlFrag is cuted
     let counterTags: number = 0;
@@ -33,40 +40,56 @@ const htmlTagSearchHelper: (htmlFrag: string) => number[] = (htmlFrag) => {
     if (tagName.length > 0) {
         counterTags = 1;
     } else {
-        return [0]
+        return [0];
     }
-    absolutIndex = htmlFrag.indexOf(tagName) + tagName.length;
+
+    let tagIndex: number = htmlFrag.indexOf(tagName);
+    if (tagIndex > 0) siblingIndexes.push(tagIndex);
+    absolutIndex = tagIndex + tagName.length;
     htmlFrag = htmlFrag.substring(absolutIndex);
+
     // Search for ocurency of tagName in string
     while (tagName.length > 0) {
         let emargencyBreak: number = 0;
         const regExp: RegExp = new RegExp(`/(?<=[</]${tagName}/`)
         while (true) {
             const indexCurrentTag: number = htmlFrag.search(regExp);
-            const indexAfterTag: number = indexCurrentTag + tagName.length;
+            const indexAfterClosingTag: number = indexCurrentTag + tagName.length + 2;
             const charBeforeTag: string | undefined = htmlFrag.at(indexCurrentTag - 1);
-            absolutIndex += indexAfterTag;
-            htmlFrag = htmlFrag.substring(indexAfterTag);
+            // Update absolutIndex and slice the htmlFrag
+            absolutIndex += indexAfterClosingTag;
+            htmlFrag = htmlFrag.substring(indexAfterClosingTag);
 
             // If it is </${tagName} substract -1 from counterTags
-            if (charBeforeTag === '/') counterTags--;
+            if (charBeforeTag === '/') {
+                counterTags--;
+            }
             // If counterTags gets 0 leave the loop
-            if (counterTags === 0) break;
+            if (counterTags === 0) {
+                break;
+            }
             // If it is <${tagName} add +1 to counter
-            if (charBeforeTag === '<') counterTags++;
-            if (emargencyBreak > 50 || !charBeforeTag) return [-1];
+            if (charBeforeTag === '<') {
+                counterTags++;
+            }
+            if (emargencyBreak > 50 || !charBeforeTag) {
+                return [-1];
+            }
             emargencyBreak++;
         }
-        // If counterTags is 0 get the actual index and add it to array
-        // Update absolutIndex and slice the htmlFrag
+        // If counterTags is 0 get the actual index and add it to the array
+        siblingIndexes.push(absolutIndex);
         // Serach for new tagName and start loop again
+        tagName = htmlFrag.substring(htmlFrag.search(/[<]/) + 1, htmlFrag.search(/(?<=\<)(?<=\w+)[\s>]/));
     }
-    return [];
+    return siblingIndexes;
 }
 
 const childCreator: (htmlFrag: string) => Array<any> = (htmlFrag) => {
     // Return if htmlFragment empty or there are no < in there
-    if (!htmlFrag.includes('<')) return [htmlFrag];
+    if (!htmlFrag.includes('<')) {
+        return [htmlFrag];
+    }
     // Take text before and behind the tag and seperate it
     const indexStartTag = htmlFrag.indexOf('<');
     const indexCloseTag = htmlFrag.lastIndexOf('>');
