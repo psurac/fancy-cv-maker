b620087c7596e7676af145c5a3d3af3d2528add8
psurac <philipsurac@web.de>
2023-10-27 22:28:39 +0300

    Started changes in the config files for jest, and reseted the node_modules, by reinstalling, because of version issues.

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-  plugins: ["@babel/plugin-transform-runtime"],
-  transformIgnorePatterns: [
-    "<rootDir>/node_modules/(?!@react-dnd|react-dnd|dnd-core|react-dnd-html5-backend)/",
-  ],
+    presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+      '@babel/preset-typescript',
+    ],
   };
