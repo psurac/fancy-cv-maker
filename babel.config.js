606efd67df8b4a5b40e259a17f770eb293e55a3a
psurac <philipsurac@web.de>
2023-11-01 12:23:36 +0200

    Solved the import issue with reactdnd and jest, babel

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,15 @@
 module.exports = {
     presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
+      ['@babel/preset-env', {
+        modules: 'commonjs',
+        loose: true,
+        targets: {node: 'current'}
+      }],
       '@babel/preset-typescript',
     ],
-    /* "plugins": [
-      ["@babel/plugin-proposal-decorators", { "version": "2023-05" }],
-      "@babel/plugin-syntax-decorators"
-    ] */
+    "plugins": [
+      /* ["@babel/plugin-proposal-decorators", { "version": "2023-05" }],
+      "@babel/plugin-syntax-decorators", */
+      "@babel/plugin-transform-react-jsx"
+    ],
   };
--- jest.config.ts
+++ jest.config.ts
@@ -3,104 +3,104 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
-import {defaults} from 'jest-config';
+import type { Config } from 'jest';
+import { defaults } from 'jest-config';
 
 const config: Config = {
   verbose: true,
   // All imported modules in your tests should be mocked automatically
   // automock: false,
-  
+
   // Stop running tests after `n` failures
   // bail: 0,
-  
+
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/private/var/folders/ts/3q8xm9610_v8wy5f2p2ls2j00000gn/T/jest_dx",
-  
+
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-  
+
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-  
+
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
-  
+
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-  
+
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
-    
-    // Indicates which provider should be used to instrument code for coverage
-    // coverageProvider: "babel",
-    
-    // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: [
-      //   "json",
-      //   "text",
-      //   "lcov",
-      //   "clover"
-      // ],
-      
-      // An object that configures minimum threshold enforcement for coverage results
-      // coverageThreshold: undefined,
-      
-      // A path to a custom dependency extractor
-      // dependencyExtractor: undefined,
-      
-      // Make calling deprecated APIs throw helpful error messages
-      // errorOnDeprecated: false,
-      
-      // The default configuration for fake timers
-      // fakeTimers: {
-        //   "enableGlobally": false
-        // },
-        
-        // Force coverage collection from ignored files using an array of glob patterns
-        // forceCoverageMatch: [],
-        
-        // A path to a module which exports an async function that is triggered once before all test suites
-        // globalSetup: undefined,
-        
-        // A path to a module which exports an async function that is triggered once after all test suites
-        // globalTeardown: undefined,
-        
-        // A set of global variables that need to be available in all test environments
-        globals: {
-          "ts-jest": {
-            babelConfig: true,
-          },
-        },
-        
-        // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-        // maxWorkers: "50%",
-        
-        // An array of directory names to be searched recursively up from the requiring module's location
-        // moduleDirectories: [
-          //   "node_modules"
-          // ],
-          
-          // An array of file extensions your modules use
-          moduleFileExtensions: [
-            "js",
-            "mjs",
-            "cjs",
-            "jsx",
-            "ts",
-            "tsx",
-            "json",
-            "node"
-          ],
-          /* moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'], */
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  // coverageProvider: "babel",
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+
+  // An object that configures minimum threshold enforcement for coverage results
+  // coverageThreshold: undefined,
+
+  // A path to a custom dependency extractor
+  // dependencyExtractor: undefined,
+
+  // Make calling deprecated APIs throw helpful error messages
+  // errorOnDeprecated: false,
+
+  // The default configuration for fake timers
+  // fakeTimers: {
+  //   "enableGlobally": false
+  // },
+
+  // Force coverage collection from ignored files using an array of glob patterns
+  // forceCoverageMatch: [],
+
+  // A path to a module which exports an async function that is triggered once before all test suites
+  // globalSetup: undefined,
+
+  // A path to a module which exports an async function that is triggered once after all test suites
+  // globalTeardown: undefined,
+
+  // A set of global variables that need to be available in all test environments
+  globals: {
+    "ts-jest": {
+      babelConfig: true,
+    },
+  },
+
+  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  // maxWorkers: "50%",
+
+  // An array of directory names to be searched recursively up from the requiring module's location
+  // moduleDirectories: [
+  //   "node_modules"
+  // ],
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+  /* moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'], */
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper : {
-    '^react-dnd$': 'react-dnd/dist/cjs',
-    '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs',
-    '^dnd-core$': 'dnd-core/dist/cjs',
+  moduleNameMapper: {
+    '^react-dnd$': 'react-dnd-cjs',
+    '^react-dnd-html5-backend$': 'react-dnd-html5-backend-cjs',
+    // '^dnd-core$': 'dnd-core/dist/cjs',
     '\\.(css|less|sass|scss)$': '<rootDir>/client/__test-help__/__mocks__/styleMock.tsx'
   },
 
@@ -190,14 +190,21 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    // "^.+\\.tsx?$": "ts-jest",
+    '\\.[jt]sx?$': 'babel-jest',
+    "<rootDir>/node_modules/(react-dnd|dnd-core|react-dnd-html5-backend)/dist/(.+).js": "ts-jest",
+    "<rootDir>/node_modules/@react-dnd/(invariant|asap|shallowequal)?/dist/(.+).js": "ts-jest",
+    "<rootDir>/client/node_modules/(react-dnd|dnd-core|react-dnd-html5-backend)/dist/(.+).js": "ts-jest",
+    "<rootDir>/client/node_modules/@react-dnd/(invariant|asap|shallowequal)?/dist/(.+).js": "ts-jest",
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "\\.pnp\\.[^\\/]+$",
+    "<rootDir>/node_modules/(?!@react-dnd|react-dnd|core-dnd|dnd-core|react-dnd-html5-backend)/",
+    "<rootDir>/client/node_modules/(?!@react-dnd|react-dnd|core-dnd|dnd-core|react-dnd-html5-backend)/"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
